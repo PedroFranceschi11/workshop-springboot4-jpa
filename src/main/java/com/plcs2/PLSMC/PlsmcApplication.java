1d3d3932d49a14ccb7afca0942c4a658f1f36f34
Pedro Franceschi <contatopedrovagas@gmail.com>
2026-07-15 15:19:46 -0300

    Project created

---
+++ src/main/java/com/plcs2/PLSMC/PlsmcApplication.java
@@ -0,0 +1,13 @@
+package com.plcs2.PLSMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlsmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlsmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plcs2/PLSMC/PlsmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plcs2.PLSMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlsmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
