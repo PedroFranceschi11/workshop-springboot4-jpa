cf27873a9a969f1347be129fc72d8ccf84b86d5b
Pedro Franceschi <contatopedrovagas@gmail.com>
2026-07-16 15:47:27 -0300

    Service layer, component registration

---
+++ src/main/java/com/plcs2/PLSMC/services/UserService.java
@@ -0,0 +1,26 @@
+package com.plcs2.PLSMC.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.plcs2.PLSMC.entities.User;
+import com.plcs2.PLSMC.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =repository.findById(id);
+		return obj.get();
+	}
+}
